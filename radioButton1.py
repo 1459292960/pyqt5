f7b3d83831abfbfbc50c9667c14ed7efb8963f1a
1459292960 <pengbin1996@foxmail.com>
2020-01-09 16:09:57 +0800

    单选按钮使用

---
+++ radioButton1.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QHBoxLayout,QRadioButton,QWidget
+
+class QRadioDemo(QWidget):
+    def __init__(self):
+        super(QRadioDemo,self).__init__()
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle('123')
+        layout=QHBoxLayout()
+
+        self.btn1=QRadioButton('单选按钮1')
+        self.btn1.setChecked(True)
+        self.btn1.toggled.connect(lambda :self.buttonState(self.btn1))
+        layout.addWidget(self.btn1)
+
+        self.btn2=QRadioButton('单选按钮2')
+        self.btn2.toggled.connect(lambda:self.buttonState(self.btn2))
+        layout.addWidget(self.btn2)
+        self.setLayout(layout)
+
+    def buttonState(self,btn):
+        if btn.isChecked()==True:
+             print(btn.text()+'被勾选')
+        else:
+            None
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    main=QRadioDemo()
+    main.show()
+    sys.exit(app.exec_())
+
+
