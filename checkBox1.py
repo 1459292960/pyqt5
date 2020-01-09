90945dbae006696e3b730383a68f35dc4a69c0a3
1459292960 <pengbin1996@foxmail.com>
2020-01-09 17:17:38 +0800

    单选按钮使用

---
+++ checkBox1.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow,QHBoxLayout,QCheckBox,QWidget
+
+class CheckBoxDemo(QMainWindow):
+    def __init__(self):
+        super(CheckBoxDemo,self).__init__()
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle('复选框测试')
+        layout=QHBoxLayout()
+
+        self.cb1=QCheckBox('复选框控件1')
+        self.cb1.setChecked(True)
+        self.cb1.stateChanged.connect(lambda : self.checkboxState(self.cb1))
+        layout.addWidget(self.cb1)
+
+        self.cb2=QCheckBox('复选框控件2')
+        self.cb2.stateChanged.connect(lambda: self.checkboxState((self.cb2)))
+        mainFrame=QWidget()
+        mainFrame.setLayout(layout)
+        self.setCentralWidget(mainFrame)
+        layout.addWidget(self.cb2)
+
+    def checkboxState(self,cb):
+        cbStatus=cb.text()+',is Checked='+str(cb.isChecked())+',checkState='+str(cb.checkState())
+        print(cbStatus+'\n')
+        if self.cb2.isChecked()==True:
+            print('想干啥干啥')
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    main=CheckBoxDemo()
+    main.show()
+    sys.exit(app.exec_())
